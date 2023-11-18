fe8a20299b8c6cfb8a9a99ea86873800c90c6908
ben20471 <ben20471@laposte.net>
2023-11-18 12:59:09 +0100

    New example
    Added a command line Batch example.

---
+++ Batch/CommandLine.bat
@@ -0,0 +1,4 @@
+@echo off
+REM The above line disable the display of commands
+echo Hello, World!
+REM "echo" displays a text, and there is no need for quotes, as the "echo" command doesn't need any arguments.
